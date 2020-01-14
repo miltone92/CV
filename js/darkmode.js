b6301da5c02308ab39404e346a4a5ef92b0427ef
milton <mpinedal@ucenfotec.ac.cr>
2020-01-14 10:31:40 -0600

    dark mode

---
+++ js/darkmode.js
@@ -0,0 +1,32 @@
+let darkMode = document.getElementById("dark-mode");
+let darkToggleTop = document.getElementById("dark-toggle-top");
+let darkToggleSide = document.getElementById("dark-toggle-side");
+let bulbSide = document.getElementById("bulb-side");
+let bulbTop = document.getElementById("bulb-top");
+
+let applyStyle = () => {
+  let href = darkMode.getAttribute("href");
+
+  if (href == "./style/style.css" || href == "none" || href == "") {
+    darkMode.href = "./style/dark.css";
+    bulbSide.classList.add("fas");
+    bulbSide.classList.remove("far");
+    bulbSide.classList.add("sun-flower");
+
+    bulbTop.classList.add("fas");
+    bulbTop.classList.remove("far");
+    bulbTop.classList.add("sun-flower");
+  } else {
+    darkMode.href = "";
+    bulbSide.classList.remove("fas");
+    bulbSide.classList.add("far");
+    bulbSide.classList.remove("sun-flower");
+
+    bulbTop.classList.add("fas");
+    bulbTop.classList.remove("far");
+    bulbTop.classList.add("sun-flower");
+  }
+};
+
+darkToggleTop.addEventListener("click", applyStyle);
+darkToggleSide.addEventListener("click", applyStyle);
