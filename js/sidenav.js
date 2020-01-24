c2c675cbf63dfc5cf0d5ab726b9669dfca043981
milton <mpinedal@ucenfotec.ac.cr>
2020-01-23 22:22:36 -0600

    navigation style change

--- js/sidenav.js
+++ js/sidenav.js
@@ -1,3 +1,7 @@
+/**
+ * Open and close side nav
+ */
+
 let sidenav = document.getElementById("side-nav");
 let closeButton = document.getElementById("close-button");
 let openButton = document.getElementById("open-sidenav-button");
@@ -16,3 +20,45 @@ let openSideMenu = () => {
 
 closeButton.addEventListener("click", closeSideMenu);
 openButton.addEventListener("click", openSideMenu);
+
+/**
+ * Highlight nav section when on section
+ */
+
+let sections = document.getElementsByClassName("portfolio-section");
+let navLinks = document.getElementsByClassName("nav-link");
+console.log(sections);
+
+let highlightSectionLinkOnMouseOver = section => {
+  let id = section.id;
+  console.log(section);
+  console.log(id);
+
+  for (const link of navLinks) {
+    link.classList.remove("active-link");
+    if (link.getAttribute("href") == `#${id}`) {
+      link.classList += " active-link";
+    }
+  }
+};
+
+let highlightSectionLinkOnClick = link$ => {
+  for (const link of navLinks) {
+    link.classList.remove("active-link");
+    if (link.getAttribute("href") == link$.getAttribute("href")) {
+      link.classList += " active-link";
+    }
+  }
+};
+
+for (const section of sections) {
+  section.addEventListener("mouseenter", () => {
+    highlightSectionLinkOnMouseOver(section);
+  });
+}
+
+for (const link of navLinks) {
+  link.addEventListener("click", () => {
+    highlightSectionLinkOnClick(link);
+  });
+}
